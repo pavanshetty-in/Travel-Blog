54411eb44a6a5e4b88b0c8faee675e1fa864238d
modimanju <modimanju2019@gmail.com>
2021-06-27 19:28:40 +0530

    Server structure defined

---
+++ server/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) =>{
+    res.send("Hello from Backend")
+})
+app.listen(port, () => {
+    console.log(`Server is running at port no ${port}`);
+})
