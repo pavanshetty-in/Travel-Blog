b60ed00bd0e690d34bf68637c85d3b3a93dbbd49
modimanju <modimanju2019@gmail.com>
2021-06-27 20:59:43 +0530

    SignInUp page frontend connected

--- server/src/app.js
+++ server/src/app.js
@@ -4,10 +4,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 require("./db/conn");
 const template_path = path.join(__dirname, "../templates/views");
+const static_path = path.join(__dirname, "../public");
+app.use(express.static(static_path));
 app.set("view engine", "hbs")
 app.set("views", template_path)
 app.get('/', (req, res) => {
     res.render("index")
+})
+app.get('/signInUp', (req, res) => {
+    res.render("signInUp")
 })
 app.listen(port, () => {
     console.log(`Server is running at port no ${port}`);
