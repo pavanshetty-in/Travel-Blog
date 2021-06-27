eb156079784960eaaa6c4e81fcdfa2aa293266da
modimanju <modimanju2019@gmail.com>
2021-06-27 20:29:17 +0530

    forntend connected

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-require("./db/conn")
-
-app.get('/', (req, res) =>{
-    res.send("Hello from Backend")
+require("./db/conn");
+const template_path = path.join(__dirname, "../templates/views");
+app.set("view engine", "hbs")
+app.set("views", template_path)
+app.get('/', (req, res) => {
+    res.render("index")
 })
 app.listen(port, () => {
     console.log(`Server is running at port no ${port}`);
